f5ae18bae1d7c09c5ec993d1360d87976a3ac14d
Tharindu-X <190408711+Tharindu-X@users.noreply.github.com>
2025-10-12 20:19:22 +0530

    added company routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes.js';
 import jobRoutes from "./routes/jobRoutes.js";
+import companyRoutes from "./routes/companyRoutes.js";
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use("/api/jobs", jobRoutes);
+app.use("/api/companies", companyRoutes);
 
 app.get('/', (req, res) => {
   res.send('✅ Backend server is running!');
