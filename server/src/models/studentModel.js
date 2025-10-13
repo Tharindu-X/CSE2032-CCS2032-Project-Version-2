54d2903f5fc9a2cedc54c9e121c6165f050007fa
kavishka venuka <kavishkavenuka554pro@gmail.com>
2025-10-13 09:55:32 +0530

    Created the student class

--- server/src/models/studentModel.js
+++ server/src/models/studentModel.js
@@ -16,14 +16,18 @@ export const StudentModel = {
 
 
 class Student {
-  constructor( email, f_name, l_name, year, password, balance=0, id = null) {
+  constructor( email, f_name, l_name, year, password, dgree, dep_name, reg_no, linkedin_url, CV, id = null) {
     this.email = email;
     this.f_name = f_name;
     this.l_name = l_name;
     this.year = year;
     this.password = password;
-    this.balance = balance;
-    this.isAdmin = isAdmin;
+    this.dgree = dgree; 
+    this.dep_name = dep_name;
+    this.reg_no = reg_no;
+    this.linkedin_url = linkedin_url;
+    this.id = id;
+    this.CV = CV;
   };
 
   }
